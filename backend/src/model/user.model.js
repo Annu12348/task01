dada19e8b2d070bee8f0e53f73be64f21dfb32db
Annu Singh <annu37752@gmail.com>
2026-09-19 16:52:05 +0530

    role fixed

--- backend/src/model/user.model.js
+++ backend/src/model/user.model.js
@@ -34,7 +34,7 @@ const userSchema = mongoose.Schema({
 
     role: {
         type: String,
-        enum: ["user", "admin", "superAdmin"],
+        enum: ["user", "admin"],
         default: "user",
     },
 
