be3b887d925f018a5741cdc9b5407d05676a9190
Annu Singh <annu37752@gmail.com>
2026-09-22 20:33:42 +0530

    fixed layout

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,7 +1,12 @@
 import ReduxProvider from "../features/redux/provider";
 import "./globals.css";
+import type { ReactNode } from "react";
 
-export default function RootLayout({ children }) {
+export default function RootLayout({ 
+  children,
+ }: {
+  children: ReactNode
+ }) {
   return (
     <html lang="en">
       <body>
